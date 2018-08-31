5fe225e29a377a01c68334730c276ce283851299
Pratik Ganvir <pratik.ganvir@ril.com>
2018-08-31 12:08:00 +0530

    Redirected admin to guitars list page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
 		if current_or_guest_user.admin?
-			new_guitar_path
+			guitars_path
 		else
 			super
 		end
